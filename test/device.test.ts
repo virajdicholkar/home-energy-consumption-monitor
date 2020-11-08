9e2b71cbd7f93e689470784f73c1b1114131bc6f
Viraj Dicholkar <dicholkar.vv@gmail.com>
2020-11-09 01:34:01 +0530

    added test cases for get device list api

--- test/device.test.ts
+++ test/device.test.ts
@@ -2,7 +2,7 @@ import app from "../server/app";
 const chai = require('chai');
 const request = require('supertest');
 
-describe.only('POST /device', async () => {
+describe('POST /device', async () => {
     let token;
     const url = '/device';
     before(async () => {
@@ -22,7 +22,7 @@ describe.only('POST /device', async () => {
             .expect(401)
     })
 
-    it.skip('Should return device token if device is registered', async () => {
+    it('Should return device token if device is registered', async () => {
         const device = {
             "name": "Hall Fan",
             "description": "This is 5 star fan",
@@ -37,7 +37,7 @@ describe.only('POST /device', async () => {
         chai.expect(deviceToken).to.be.a('string');
     })
 
-    it.skip('Should return validation error if device name is already registered', async () => {
+    it('Should return validation error if device name is already registered', async () => {
         const device = {
             "name": "Tube light",
             "description": "This is 5 star fan",
@@ -67,7 +67,7 @@ describe.only('POST /device', async () => {
             "description": "This is 5 star fan",
             "powerInWatts": 3000
         }
-        
+
         const response2 = await request(app)
             .post(url)
             .send(device)
@@ -86,7 +86,7 @@ describe.only('POST /device', async () => {
             "description": "This is 5 star fan",
             "powerInWatts": ''
         }
-        
+
         const response2 = await request(app)
             .post(url)
             .send(device)
@@ -99,5 +99,52 @@ describe.only('POST /device', async () => {
         chai.expect(errorMessage).to.equal('Validation error!');
     })
 
-   
+})
+
+describe('GET /device', async () => {
+    let token;
+    const url = '/device';
+    before(async () => {
+        const body = {
+            "loginName": "testinghome2",
+            "password": "testhome@123"
+        }
+        let login = await request(app)
+            .post('/auth/login')
+            .send(body);
+        token = login.body.token
+    })
+
+    it('Should return unauthorized error if token not passed in headers', async () => {
+        const response = await request(app)
+            .post(url)
+            .expect(401)
+    })
+
+    it('Should return registered device list with length smaller than 10', async () => {
+
+        const response = await request(app)
+            .get(url)
+            .set('authorization', token)
+            .expect(200)
+        const { result, totalCount } = response.body;
+        chai.expect(result).to.be.an('array')
+        chai.expect(totalCount).to.be.a('number')
+        chai.expect(result.length <= 10).to.equal(true);
+        chai.expect(result.length <= totalCount).to.equal(true);
+    })
+
+    it('If limit is passed to url query, API should return array of logs with length length smaller that or equal to limit ', async () => {
+        const limit = 5;
+        const urlWithQuery = `${url}?limit=${limit}`
+        const response = await request(app)
+            .get(url)
+            .set('authorization', token)
+            .expect(200)
+        const { result, totalCount } = response.body;
+        chai.expect(result).to.be.an('array')
+        chai.expect(totalCount).to.be.a('number')
+        chai.expect(result.length <= limit).to.equal(true);
+        chai.expect(result.length <= totalCount).to.equal(true);
+    })
 })
